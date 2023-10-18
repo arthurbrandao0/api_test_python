1a821d884b576f285168f6bfaf267579373a9340
FRAMEARTHUR\Arthur <arthurbm15@gmail.com>
2023-10-18 15:07:30 -0300

    Optimizing scenarios

--- features/login.feature
+++ features/login.feature
@@ -1,8 +1,8 @@
 Feature: Login in Swab Labs
 
   Scenario Outline: Successfull login
-    Given the user is on login page
-    When the user enter the username "<username>" and the password "<password>"
+    Given the user is on the login page
+    When the user enters the username "<username>" and the password "<password>"
     And the user clicks the login button
     Then the user is redirected to the inventory page
 
@@ -13,8 +13,8 @@ Feature: Login in Swab Labs
       | performance_glitch_user | secret_sauce |
 
   Scenario: Successfull logout
-    Given the user is on login page
-    When the user enter the username "standard_user" and the password "secret_sauce"
+    Given the user is on the login page
+    When the user enters the username "standard_user" and the password "secret_sauce"
     And the user clicks the login button
     Then the user is redirected to the inventory page
     When the user clicks the menu button
@@ -23,15 +23,15 @@ Feature: Login in Swab Labs
 
   @test3
   Scenario: Invalid login with wrong password
-    Given the user is on login page
-    When the user enter the username "standard_user" and the password "1"
+    Given the user is on the login page
+    When the user enters the username "standard_user" and the password "1"
     And the user clicks the login button
     Then error message "Epic sadface: Username and password do not match any user in this service" is displayed
 
 
   Scenario: Invalid login with blocked user
-    Given the user is on login page
-    When the user enter the username "locked_out_user" and the password "secret_sauce"
+    Given the user is on the login page
+    When the user enters the username "locked_out_user" and the password "secret_sauce"
     And the user clicks the login button
     Then error message "Epic sadface: Sorry, this user has been locked out." is displayed
 
--- features/steps/login.py
+++ features/steps/login.py
@@ -1,15 +1,17 @@
 from behave import given, when, then
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support import expected_conditions
 
 
-@given('the user is on login page')
+# Given
+@given('the user is on the login page')
 def step_user_on_login_page(context):
     context.driver.get('https://www.saucedemo.com/')
 
 
-@when('the user enter the username "{username}" and the password "{password}"')
+# When
+@when('the user enters the username "{username}" and the password "{password}"')
 def step_user_enters_credentials(context, username, password):
     username_field = context.driver.find_element(By.ID, 'user-name')
     password_field = context.driver.find_element(By.ID, 'password')
@@ -26,26 +28,29 @@ def step_user_clicks_login_button(context):
 
 @when('the user clicks the menu button')
 def step_user_clicks_menu_button(context):
-    login_button = context.driver.find_element(By.ID, 'react-burger-menu-btn')
-    login_button.click()
+    menu_button = context.driver.find_element(By.ID, 'react-burger-menu-btn')
+    menu_button.click()
 
 
 @when('the user clicks the logout button')
 def step_user_clicks_logout_button(context):
-    login_button = WebDriverWait(context.driver, 5).until(
-        ec.element_to_be_clickable((By.ID, 'logout_sidebar_link'))
+    logout_button = WebDriverWait(context.driver, 5).until(
+        expected_conditions.element_to_be_clickable((By.ID, 'logout_sidebar_link'))
     )
-    login_button.click()
+    logout_button.click()
 
 
+# Then
 @then('the user is redirected to the inventory page')
 def step_user_redirected_to_inventory_page(context):
-    assert context.driver.current_url == 'https://www.saucedemo.com/inventory.html'
+    inventory_url = 'https://www.saucedemo.com/inventory.html'
+    WebDriverWait(context.driver, 5).until(expected_conditions.url_to_be(inventory_url))
 
 
 @then('the user is redirected to the home page')
 def step_user_redirected_to_homepage(context):
-    assert context.driver.current_url == 'https://www.saucedemo.com/'
+    homepage_url = 'https://www.saucedemo.com/'
+    WebDriverWait(context.driver, 5).until(expected_conditions.url_to_be(homepage_url))
 
 
 @then('error message "{message}" is displayed')
